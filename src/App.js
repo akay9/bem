b4fb91199e28fce3115e389f6680f376da1649a3
akay9 <82910848+akay9@users.noreply.github.com>
2021-04-20 17:19:37 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <h1 className="title">Big Easy Moon</h1>
+        <div className="moon"></div>
+        <div className="info">
+          {/* <h3>The next /biz/ moonshot</h3> */}
+          <h5>almost before we knew it, we had left the ground...</h5>
+          {/* <br></br> */}
+          <h5>the spectacle before us was indeed sublime</h5>
+        </div>
+      </div>
+    </>
+  );
+}
